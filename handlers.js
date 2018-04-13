301f184fed66b4e1c291ea196616cab44e0de9d2
Honza Kotrs <honza@kotrs.cz>
2018-04-13 18:22:14 +0200

    Added start/stop/restart routes

---
+++ handlers.js
@@ -0,0 +1,83 @@
+'use strict';
+const DARK_ICE = 'darkice';
+
+let ctl = require('sysctlx');
+let http = require('http');
+
+function parseEnableResponse(raw) {
+	return raw === '' || raw.match(/created symlink/i) !== null;
+}
+
+function parseDisableResponse(raw) {
+	return raw === '' || raw.match(/removed/i) !== null;
+}
+
+function sendError(response, message) {
+	response.status(400);
+	response.send(message);
+}
+function sendOk(response) {
+	response.status(200);
+	response.json({result: true});
+}
+
+exports.enableDarkIce = function (request, response) {
+	ctl.enable(DARK_ICE).then(function (result) {
+		console.log(result);
+		if (parseEnableResponse(result)) {
+			sendOk(response);
+		} else {
+			sendError(response, 'Unable to enable service!');
+		}
+	});
+};
+
+exports.disableDarkIce = function (request, response) {
+	ctl.disable(DARK_ICE).then(function (result) {
+		console.log(result);
+		if (parseDisableResponse(result)) {
+			sendOk(response);
+		} else {
+			sendError(response, 'Unable to disable service!');
+		}
+	});
+};
+
+exports.startDarkIce = function (request, response) {
+	ctl.start(DARK_ICE).then(function (result) {
+		sendOk(response);
+	});
+};
+
+exports.stopDarkIce = function (request, response) {
+	ctl.stop(DARK_ICE).then(function (result) {
+		sendOk(response);
+	});
+};
+
+exports.restartDarkIce = function (request, response) {
+	ctl.restart(DARK_ICE).then(function (result) {
+		sendOk(response);
+	});
+};
+
+exports.getDarkIceStatus = function (request, response) {
+	ctl.status(DARK_ICE).then(function (status) {
+		response.json(status);
+	}).catch(function (err) {
+		sendError(response, err);
+	});
+};
+
+exports.getStreamStatus = function (request, response) {
+	let streamUrl = request.query.s;
+	if (s != null && s !== '') {
+		http.get(streamUrl, function (res) {
+			response.json({
+				status: res.statusCode === 200
+			});
+		});
+	} else {
+		sendError(response, 'No stream URL provided, please set the stream URL to \'s\' parameter');
+	}
+};
--- routes.js
+++ routes.js
@@ -1,12 +1,13 @@
 'use strict';
 module.exports = function(server) {
-	var controller = require('./controller.js');
+	var handlers = require('./handlers.js');
 
 	// todoList Routes
-	server.get('/darkice', controller.getDarkIceStatus);
-    server.get('/darkice/enable', controller.enableDarkIce);
-    server.get('/darkice/disable', controller.disableDarkIce);
-    server.get('/darkice/start', controller.startDarkIce);
-    server.get('/darkice/stop', controller.stopDarkIce);
-    server.get('/darkice/restart', controller.restartDarkIce);
+	server.get('/darkice', handlers.getDarkIceStatus);
+	server.get('/darkice/enable', handlers.enableDarkIce);
+	server.get('/darkice/disable', handlers.disableDarkIce);
+	server.get('/darkice/start', handlers.startDarkIce);
+	server.get('/darkice/stop', handlers.stopDarkIce);
+	server.get('/darkice/restart', handlers.restartDarkIce);
+	server.get('/stream', handlers.getStreamStatus);
 };
